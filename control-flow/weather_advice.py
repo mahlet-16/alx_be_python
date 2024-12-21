efa5a955ca2462d1099f52c03a7bb9da2d6c64e0
mahlet16 <mahletamenu16@gmail.com>
2024-12-20 22:45:00 -0800

    weather advise

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -4,7 +4,7 @@ if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
 elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif weather == "clod":
+elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
