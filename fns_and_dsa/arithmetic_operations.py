1aa8032af9e473bd95ff1269b0228bd253e39925
mahlet16 <mahletamenu16@gmail.com>
2025-01-10 13:11:37 -0800

    arithmetic operation

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,14 @@
 def perform_operation(num1: float, num2: float, operation: str):
      if operation == "add":
         return num1 + num2
-    elif operation == "subtract":
+     elif operation == "subtract":
         return num1 - num2
-    elif operation == "multiply":
+     elif operation == "multiply":
         return num1 * num2
-    elif operation == "divide":
+     elif operation == "divide":
         if num2 == 0:
             return "Error: Division by zero is not allowed."
         else:
             return num1 / num2
-    else:
+     else:
         return "Error: Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'." 
