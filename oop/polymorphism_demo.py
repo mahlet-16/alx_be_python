309430aead8303a919c6e7980b1505b5599112cc
mahlet16 <mahletamenu16@gmail.com>
2025-01-24 05:15:21 -0800

    polymorphism demo

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,15 @@
+class Shape:
+    def area(self):
+        raise "NotImplimentedError"
+class Rectangle(Shape):
+    def rect(self, length, width):
+        self.length = length
+        delf.width = width
+        area.rect = self.length * self.width
+class Circle(Shape):
+    def circ(self, radius):
+        self.radius = radius
+        area.circ = math.pi * self.radius * self.radius
+       
+    
+    
