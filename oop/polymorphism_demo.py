066184bef04f3f92ce8c751785fc9f22478fe7b7
mahlet16 <mahletamenu16@gmail.com>
2025-01-24 05:26:43 -0800

    polymorphism demo

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -3,13 +3,15 @@ class Shape:
     def area(self):
         raise NotImplimentedError("Subclass should impliment this method")
 class Rectangle(Shape):
-    def area(self, length, width):
+    def __init__(self, length, width):
         self.length = length
         self.width = width
+    def area(self):
         return self.length * self.width
 class Circle(Shape):
-    def area(self, radius):
+    def __init__(self, radius):
         self.radius = radius
+    def area(self):
         return math.pi * self.radius ** 2
        
     
