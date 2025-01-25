8b7c81ef5188cdaf4428b19ca1461156a2035fd1
mahlet-16 <mahletamenu16@gmail.com>
2025-01-25 16:02:39 +0300

    Update class_static_methods_demo.py

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -2,9 +2,9 @@ class Calculator:
     calculation_type = "Arithmetic Operations"
 
 @staticmethod
-    def add(a, b):
-         return a + b
- @classmethod
-    def multiply(cls, a, b):
-         print(f"Calculation type: {calculation_type}")
-         return a * b
+def add(a, b):
+    return a + b
+@classmethod
+def multiply(cls, a, b):
+    print(f"Calculation type: {calculation_type}")
+    return a * b
